c88892ac25c1b0f90ff65e0b3a682428bff9e093
wyrmss <62921394+wyrmss@users.noreply.github.com>
2020-04-04 05:24:07 +0200

    Update install_open_corsair.sh
    Añadir un menú al instalador y incluir la desinstalación.

--- install_open_corsair.sh
+++ install_open_corsair.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
+
+clear
+while :
+do
+echo " Escoja una opcion "
+echo "1. Instalar dependencias?"
+echo "2. Instalar"
+echo "3. Crear acceso directo?"
+echo "4. Desinstalar"
+echo "5. Salir"
+echo -n "Seleccione una opcion [1 - 5]"
+read opcion
+case $opcion in
+1) echo "Instalando...:";
 echo
 echo "Instalando dependencias"...
 # Instalar las  dependencias
 sudo zypper in libusb-1_0-0 pkgconf-pkg-config git
-
+;;
+2) echo "Instalando...";
 echo
 echo "Descargando el repositorio"
 # Descargar el repositorio
 echo
 git clone https://github.com/audiohacked/OpenCorsairLink.git
-
-# Preguntar si se procede en la instalación
 echo
-while true; do
-echo
-read -p "¿Quiere realizar la instalación?" yn
-case $yn in
-si ) break;;
-no ) exit;;
-* ) echo "por favor responda si o no";;
-esac
-done
-
-echo
-echo "si se ejecuta esto es que aceptaste"
-echo
-echo "Presiona una tecla para continuar...";
-read 
 
 # Ejecutar la instalación
 mv -i OpenCorsairLink/ /home/$USER/bin/OpenCorsairLink
@@ -36,6 +34,11 @@ cp -i opencorsairlink.png /home/$USER/bin/OpenCorsairLink/opencorsairlink.png
 cd /home/$USER/bin/OpenCorsairLink
 chmod +x ver-tiempo-real.sh
 
+echo
+# Ejecutar ayuda del programa
+sudo ./OpenCorsairLink.elf --device=0 --fan channel=1,mode=6,temps=31:33:36:38,speeds=30:55:65:100
+sudo ./OpenCorsairLink.elf --device=0 --fan channel=0,mode=6,temps=31:33:36:38,speeds=30:55:65:100 --led channel=0,mode=5,colors=00FF00:FFFF00:FF0000,temps=33:36:39 --pump mode=4
+sudo ./OpenCorsairLink.elf --help
 echo
 # Compilación del programa
 make
@@ -47,29 +50,31 @@ cd /home/$USER/bin/OpenCorsairLink/
 sudo ./OpenCorsairLink.elf --device=0 --fan channel=1,mode=6,temps=31:33:36:38,speeds=30:55:65:100
 sudo ./OpenCorsairLink.elf --device=0 --fan channel=0,mode=6,temps=31:33:36:38,speeds=30:55:65:100 --led channel=0,mode=5,colors=00FF00:FFFF00:FF0000,temps=33:36:39 --pump mode=4" >> /home/$USER/.config/autostart-scripts/OpenCorsairLink.sh
 chmod +x /home/$USER/.config/autostart-scripts/OpenCorsairLink.sh
+echo
+echo "Instalacióm terminada"
+echo
+;;
+3) cp -i OpenCorsairLink.desktop /home/$USER/Escritorio/OpenCorsairLink.desktop
+echo
+echo "Acceso directo creado"
+echo
+;;
+4) echo "Desinstalando..."
+cd /home/$USER/bin/OpenCorsairLink
+sudo make uninstall
+rm -vrf /home/$USER/.config/autostart-scripts/OpenCorsairLink.sh
+rm -vrf /home/$USER/Escritorio/OpenCorsair
+rm -vrf /home/$USER/bin/OpenCorsairLink/ver-tiempo-real.sh
+rm -vrf /home/$USER/bin/OpenCorsairLink/
+echo
+echo "Desinstalación terminada"
+exit 1
+;;
+5) echo "chao";
+exit 1;;
+*) echo "$opc es una opcion invalida. Es tan dificil?";
+echo "Presiona una tecla para continuar...";
+read foo;;
+esac
+done
 
-echo "[Desktop Entry]
-Comment[es_ES]=Controlador de la refrigeracion liquida Corsair
-Comment=Controlador de la refrigeracion liquida Corsair
-Exec=/home/wyrms/bin/OpenCorsairLink/ver-tiempo-real.sh
-GenericName[es_ES]=Controlador de la refrigeracion liquida Corsair
-GenericName=Controlador de la refrigeracion liquida Corsair
-Icon=/home/wyrms/bin/OpenCorsairLink/opencorsairlink.png
-MimeType=
-Name[es_ES]=OpenCorsairLink
-Name=OpenCorsairLink
-Path=/home/wyrms/bin/OpenCorsairLink
-StartupNotify=true
-Terminal=true
-TerminalOptions=\s--noclose
-Type=Application
-X-DBUS-ServiceName=
-X-DBUS-StartupType=none
-X-KDE-SubstituteUID=false
-X-KDE-Username=
-" >> /home/$USER/Escritorio/OpenCorsair
-
-# Ejecutar ayuda del programa
-sudo ./OpenCorsairLink.elf --device=0 --fan channel=1,mode=6,temps=31:33:36:38,speeds=30:55:65:100
-sudo ./OpenCorsairLink.elf --device=0 --fan channel=0,mode=6,temps=31:33:36:38,speeds=30:55:65:100 --led channel=0,mode=5,colors=00FF00:FFFF00:FF0000,temps=33:36:39 --pump mode=4
-sudo ./OpenCorsairLink.elf --help
